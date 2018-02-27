c53892bb8a4a4e6a89d2509d5aa6875fcdb210fd
Ivan <nikitinivan@protonmail.com>
2018-02-27 20:48:23 +0300

    Added BlogPost application

--- Blog/settings.py
+++ Blog/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # MY APPS
+    'blogpost',
+    # Django APPS
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
