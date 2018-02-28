6945f9eecc4b6b30c1802e0766f5ff0959e3edaa
Ivan <nikitinivan@protonmail.com>
2018-02-28 17:30:17 +0300

    Added modeladmin for post model

--- blogpost/admin.py
+++ blogpost/admin.py
@@ -6,7 +6,7 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publish', 'status')
     list_filter = ('status', 'created', 'publish', 'author')
     search_fields = ('title', 'body')
-    prepopulated_fields = {'slug': ('title','created',)}
+    prepopulated_fields = {'slug': ('title',)}
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
     ordering = ['status', 'publish']
